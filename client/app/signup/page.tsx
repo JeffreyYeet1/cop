526d16989576fb60fcc6b79d333e7dfe2dd7dde7
Terence Yang <terenceyang@Terences-MacBook-Air.local>
2025-03-22 05:04:21 -0400

    login/signup

---
+++ client/app/signup/page.tsx
@@ -0,0 +1,23 @@
+import { SignUpForm } from "../components/signup-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Create a new account",
+};
+
+export default function SignUpPage() {
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
+          <p className="text-sm text-muted-foreground">
+            Create an account to get started
+          </p>
+        </div>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
